39fe4d12b3d83ac1199faf593c1f1fcfaefc8130
Mihai Blebea <mihaiserban.blebea@gmail.com>
2021-10-18 19:10:39 +0100

    preserve home path

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "starting the deploy process..."
-ssh silverpi "curl https://raw.githubusercontent.com/MihaiBlebea/twitter_bot/master/installer.sh --output installer.sh --silent && chmod +x ./installer.sh && ./installer.sh"
+ssh silverpi "curl https://raw.githubusercontent.com/MihaiBlebea/twitter_bot/master/installer.sh --output installer.sh --silent && chmod +x ./installer.sh && sudo --preserve-env=HOME ./installer.sh"
 echo "finished!"
