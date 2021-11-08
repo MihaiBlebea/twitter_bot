b54be55b201aeaf02f69d1eebadceb78d1284ad5
Mihai Blebea <mihaiserban.blebea@gmail.com>
2021-11-08 10:59:50 +0000

    Adds retweet script

--- src/retweet.py
+++ src/retweet.py
@@ -1,24 +1,57 @@
 import tweepy
 from dotenv import dotenv_values
+import argparse
 
 from telegram import send_message
 
+class Retweet():
+
+	def __init__(self):
+		# create the config from env file
+		self.config = dotenv_values(".env")
+
+		# create the api client
+		auth = tweepy.OAuthHandler(self.config["CONSUMER_KEY"], self.config["CONSUMER_SECRET"])
+		auth.set_access_token(self.config["ACCESS_TOKEN"], self.config["ACCESS_TOKEN_SECRET"])
+
+		self.api = tweepy.API(auth)
+
+	def retweet(self, tag: str):
+		tweets = self.api.search_tweets(q=tag, lang="en", result_type="popular")
+
+		# check if the tweets are older then the last published one
+
+		# pick one tweet which meets the criteria
+		tweet = tweets[0]
+
+		# retweet it
+		tweet.retweet()
+
+		# save the retweet to the db
+
+		# send update to telegram
+		message = f"RT: {tweet.text}"
+		send_message(message, self.config["BOT_TOKEN"], self.config["CHAT_ID"])
+
 
 def main():
-	config = dotenv_values(".env")
+	parser = argparse.ArgumentParser(
+		prog= "twitter_bot", 
+		usage="%(prog)s [options]", 
+		description="post retweet on twitter.",
+	)
 
-	print("starting...")
-	auth = tweepy.OAuthHandler(config["CONSUMER_KEY"], config["CONSUMER_SECRET"])
-	auth.set_access_token(config["ACCESS_TOKEN"], config["ACCESS_TOKEN_SECRET"])
+	parser.add_argument(
+		"-q",
+		dest="query",
+		required=True, 
+		help="query for the tag to search latest tweets",
+	)
 
-	api = tweepy.API(auth)
-	tweets = api.search_tweets(q="#golang", lang="en", result_type="popular")
-	tweet = tweets[0]
+	args = parser.parse_args()
 
-	tweet.retweet()
-
-	message = f"RT: {tweet.text}"
-	send_message(message, config["BOT_TOKEN"], config["CHAT_ID"])
+	retweeter = Retweet()
+	retweeter.retweet(args.query)
 	
 
 if __name__ == "__main__":
