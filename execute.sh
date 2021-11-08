ff2d60e957fc7c12e054c9a79ff76447adae089b
Mihai Blebea <mihaiserban.blebea@gmail.com>
2021-11-08 09:11:27 +0000

    Update script to just post a message update

--- execute.sh
+++ execute.sh
@@ -1,30 +1,4 @@
 #!/bin/bash
 
-if [ "$1" == "publish" ]; then 
-	echo "publishing..."
-	eval "${PWD}/virtualenv/bin/python3 ${PWD}/src/publish.py \"${@:2}\""
-	echo "done"
+eval "${PWD}/virtualenv/bin/python3 ${PWD}/src/execute.py \"${@}\""
 
-elif [ "$1" == "schedule" ]; then
-	echo "scheduling..."
-	eval "${PWD}/virtualenv/bin/python3 ${PWD}/src/content.py \"${@:2}\""
-	echo "done"
-
-elif [ "$1" == "report" ]; then
-	echo "sending report..."
-	eval "${PWD}/virtualenv/bin/python3 ${PWD}/src/daily_report.py \"${@:2}\""
-	echo "done"
-
-elif [ "$1" == "followers" ]; then
-	echo "sending report..."
-	eval "${PWD}/virtualenv/bin/python3 ${PWD}/src/followers.py \"${@:2}\""
-	echo "done"
-
-elif [ "$1" == "retweet" ]; then
-	echo "retweeting..."
-	eval "${PWD}/virtualenv/bin/python3 ${PWD}/src/retweet.py \"${@:2}\""
-	echo "done"
-
-else
-	echo "could not find command: $1"
-fi
